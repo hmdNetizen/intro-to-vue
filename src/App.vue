67acd78e531084dd1165491e79b4f73404a7589e
Hamed Jimoh <58299107+hmdNetizen@users.noreply.github.com>
2021-01-28 06:20:24 -0800

    Completed basic v- attributes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+    <h2 v-for="fullName in fullNames" :key="fullName">
+      {{ fullName.first }} {{ fullName.last }}
+    </h2>
+    <div v-for="actor in actors" :key="actor.name">
+      <h2>{{ actor.name }}</h2>
+      <h4 v-for="movie in actor.movies" :key="movie">{{ movie }}</h4>
+    </div>
+    <h2 v-for="(value, key, index) in myInfo" :key="value">
+      {{ key }} {{ index }} {{ value }}
+    </h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // components: {
+  //   HelloWorld,
+  // },
+  data() {
+    return {
+      num: "hello",
+      display: true,
+      show: false,
+      names: ["Hamed", "Kazeem", "Idris"],
+      fullNames: [
+        { first: "Hamed", last: "Jimoh" },
+        { first: "Kazeem", last: "Raheem" },
+        { first: "Jamiu", last: "Saka" },
+      ],
+      actors: [
+        { name: "Di Caprio", movies: ["Titanic", "Wolf of Wall Street"] },
+        { name: "Dwayne Johnson", movies: ["Lion King", "Inception"] },
+      ],
+      myInfo: {
+        name: "Hameed",
+        role: "Frontend Developer",
+        stack: "ReactJS",
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +53,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.italics {
+  font-style: italic;
+}
+
+.soldout {
+  color: red;
+}
+.new {
+  color: cyan;
 }
 </style>
